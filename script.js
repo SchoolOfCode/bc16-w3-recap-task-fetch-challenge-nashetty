6a0b90711d346c645cad838ef7cf4fa481f366b1
Nada Shetty <nada.shetty@yahoo.com>
2024-02-24 22:45:37 +0000

    add additional images and search field

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function fetchData(endpoint) {
 
     // parse to json
     const data = await response.json()
-    console.log(data)
+    // console.log(data)
     return data
 }
 
@@ -28,7 +28,7 @@ async function fetchData(endpoint) {
 // Carina nebula Endpoint
 const endpoint1 = '/search?q=carina_nebula'
 const endpoint2 = '/search?q=GSFC_20171208_Archive_e002076'
-const endpoint3 = '/search?q=JPL-20231222-SOLSYSf-0001-NASA%20Telescopes%20Reveal%20Hidden%20Universe'
+const endpoint3 = '/search?q=JPL-20231222-SOLSYSf-0001-NASA Telescopes Reveal Hidden Universe'
 
 fetchData(endpoint1)
 fetchData(endpoint2)
@@ -36,25 +36,42 @@ fetchData(endpoint3)
 
 // Display data on the page
 async function displayOnPage() {
-
+    let NASAdata = []
     // get data from API
-    let NASAdata = await fetchData(endpoint1)
+    NASAdata.push(await fetchData(endpoint1))
+    NASAdata.push(await fetchData(endpoint2))
+    NASAdata.push(await fetchData(endpoint3))
 
-    // get title, img and description div from DOM
-    const title = document.querySelector('.title')
-    const description = document.querySelector('.description')
-    const displayImage = document.querySelector('.displayImage')
-    
-    // create and update text content for title
-    let text = NASAdata.collection.items[0].data[0].title
-    title.textContent = text
-    
-    // create and update text content for description
-    text = NASAdata.collection.items[0].data[0].description
-    description.textContent = text
+    // get displayResults element from DOM
+    const main = document.querySelector('main')
 
-    // display image
-    displayImage.src = NASAdata.collection.items[0].links[0].href
+    for(let i = 0; i < NASAdata.length; i++) {
+        // create elements that are the same as title, img and description in displayResults div from DOM
+        const displayResults = document.createElement('div')
+        displayResults.classList.add('displayResults')
+        const title = document.createElement('h2')
+        title.classList.add('title')
+        const description = document.createElement('div')
+        description.classList.add('description')
+        const displayImage = document.createElement('img')
+        displayImage.classList.add('displayImage')
+        
+        // create and update text content for title
+        let text = NASAdata[i].collection.items[0].data[0].title
+        title.textContent = text
+        displayResults.appendChild(title)
+
+        // display image
+        displayImage.src = NASAdata[i].collection.items[0].links[0].href
+        displayResults.appendChild(displayImage)
+        
+        // create and update text content for description
+        text = NASAdata[i].collection.items[0].data[0].description
+        description.textContent = text
+        displayResults.appendChild(description)
+        main.appendChild(displayResults)
+
+    }
     
 }
 displayOnPage()
