7ad3d470af70c869da38e139b00a630406f436c0
Nada Shetty <nada.shetty@yahoo.com>
2024-03-02 19:17:33 +0000

    search added

--- script.js
+++ script.js
@@ -1,77 +1,91 @@
 // fetch data and console log the object ✅
 // after testing in POSTMAN, the API key is not necessary
 
-// url for NASA API
-const url = 'https://images-api.nasa.gov'
-
 // function to fetch the data
-async function fetchData(endpoint) {
-    const response = await fetch(url+endpoint, {
-        headers: {
-            accept: 'application/json',
-        }
-    });
+async function fetchData() {
+  // add search functionality
+  // get search input value
+  const searchInputValue = document.querySelector("#search").value;
 
-    // response failed
-    if(!response) {
-        console.error('Request failed');
-        return
-    }
+  // url for NASA API with the search input value
+  // encode a URI (Uniform Resource Identifier) - searchInputValue before it is appended to the URL
+  const url = `https://images-api.nasa.gov/search?q=${encodeURIComponent(
+    searchInputValue
+  )}`;
 
-    // parse to json
-    const data = await response.json()
-    // console.log(data)
-    return data
+  const response = await fetch(url, {
+    headers: {
+      accept: "application/json",
+    },
+  });
+
+  // response failed
+  if (!response) {
+    console.error(`Request failed: ${response.statusText}`);
+    return;
+  }
+
+  // parse to json
+  const data = await response.json();
+  // console.log(data)
+  return data;
 }
-
-// Endpoint should be in format - /search?q={q}
-// Carina nebula Endpoint
-const endpoint1 = '/search?q=carina_nebula'
-const endpoint2 = '/search?q=GSFC_20171208_Archive_e002076'
-const endpoint3 = '/search?q=JPL-20231222-SOLSYSf-0001-NASA Telescopes Reveal Hidden Universe'
-
-fetchData(endpoint1)
-fetchData(endpoint2)
-fetchData(endpoint3)
 
 // Display data on the page
-async function displayOnPage() {
-    let NASAdata = []
-    // get data from API
-    NASAdata.push(await fetchData(endpoint1))
-    NASAdata.push(await fetchData(endpoint2))
-    NASAdata.push(await fetchData(endpoint3))
-
-    // get displayResults element from DOM
-    const main = document.querySelector('main')
-
-    for(let i = 0; i < NASAdata.length; i++) {
-        // create elements that are the same as title, img and description in displayResults div from DOM
-        const displayResults = document.createElement('div')
-        displayResults.classList.add('displayResults')
-        const title = document.createElement('h2')
-        title.classList.add('title')
-        const description = document.createElement('div')
-        description.classList.add('description')
-        const displayImage = document.createElement('img')
-        displayImage.classList.add('displayImage')
-        
-        // create and update text content for title
-        let text = NASAdata[i].collection.items[0].data[0].title
-        title.textContent = text
-        displayResults.appendChild(title)
-
-        // display image
-        displayImage.src = NASAdata[i].collection.items[0].links[0].href
-        displayResults.appendChild(displayImage)
-        
-        // create and update text content for description
-        text = NASAdata[i].collection.items[0].data[0].description
-        description.textContent = text
-        displayResults.appendChild(description)
-        main.appendChild(displayResults)
-
+function displayOnPage() {
+  // get searchBtn and input field  to add event listener to fetch data when the search button is clicked/enter pressed
+  const searchBtn = document.querySelector("#search-btn");
+  const searchInput = document.querySelector("#search");
+  // enable Enter key shortcut to press the searchBtn
+  searchInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault;
+      searchBtn.click();
     }
-    
+  });
+  // add event listener to fetch data when the search button is clicked/enter pressed
+  searchBtn.addEventListener("click", renderData);
 }
-displayOnPage()
+
+displayOnPage();
+
+async function renderData() {
+  const NASAdata = await fetchData();
+  console.log(NASAdata.collection.items.length);
+  console.log(NASAdata);
+
+  // get displayResults element from DOM and clear content
+  const main = document.querySelector("main");
+  main.innerHTML = "";
+
+  // loop through every element in the NASAdata collection
+  for (let i = 0; i < NASAdata.collection.items.length; i++) {
+    // create elements that are the same as title, img and description in displayResults div from DOM
+    const displayResults = document.createElement("div");
+    displayResults.classList.add("displayResults");
+
+    const title = document.createElement("h2");
+    title.classList.add("title");
+
+    const description = document.createElement("div");
+    description.classList.add("description");
+
+    const displayImage = document.createElement("img");
+    displayImage.classList.add("displayImage");
+
+    // create and update text content for title
+    let text = NASAdata.collection.items[i].data[0].title;
+    title.textContent = text;
+    displayResults.appendChild(title);
+
+    // display image
+    displayImage.src = NASAdata.collection.items[i].links[0].href;
+    displayResults.appendChild(displayImage);
+
+    // create and update text content for description
+    text = NASAdata.collection.items[i].data[0].description;
+    description.textContent = text;
+    displayResults.appendChild(description);
+    main.appendChild(displayResults);
+  }
+}
